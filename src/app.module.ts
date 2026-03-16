455cf367df0056cf592727ea58cb11334b3e4cb4
utkarshgangwar <utkarshgangwar22@gmail.com>
2026-03-16 18:20:22 +0530

    .env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { Module } from '@nestjs/common';
 // import { MongooseModule } from '@nestjs/mongoose';
 import { SessionsModule } from './sessions/sessions.module';
 import { MongoModule } from './database/mongo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongoModule,
     SessionsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/database/mongo.module.ts
+++ src/database/mongo.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common'
-import { MongooseModule } from '@nestjs/mongoose'
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const uri = process.env?.DB_URL || 'mongodb://localhost/';
+const dbName = process.env?.DB_NAME || 'convo-session-service';
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/convo-session-service')
-  ]
+  imports: [MongooseModule.forRoot(uri + dbName)],
 })
 export class MongoModule {}

f0e1dfeb1c91c035caddec2b0104565009f39b48
utkarshgangwar <utkarshgangwar22@gmail.com>
2026-03-17 10:43:04 +0530

    code check and fixes

--- src/database/mongo.module.ts
+++ src/database/mongo.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-const uri = process.env?.DB_URL || 'mongodb://localhost/';
-const dbName = process.env?.DB_NAME || 'convo-session-service';
+const uri = process.env.DB_URL || 'mongodb://localhost/';
+const dbName = process.env.DB_NAME || 'convo-session-service';
 @Module({
   imports: [MongooseModule.forRoot(uri + dbName)],
 })
